7a2e269002ad7aa79eef2a4206bd1b0f86d94cd3
Adarsh096 <adarshofficial11@gmail.com>
2021-11-09 10:22:11 +0530

    express nodejs code cloned

---
+++ nodejs_freecodecamp/10_node_express_intro_a.js
@@ -0,0 +1,16 @@
+// payload is the body of the request
+
+//request line is :
+/*
+GET /contact/zone1 HTTP/1.1
+    Headers
+    Body(Not for GET)
+*/
+
+// response line is :
+/*
+HTTP/1.1 200 OK
+    Header (content-type)
+    Body
+*/
+
